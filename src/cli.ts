0d73657b738619cdb60196b07f29ad2170cdd58f
Thomas F. K. Jorna <hello@tefkah.com>
2022-08-22 14:22:54 +0200

    remove json export

--- src/cli.ts
+++ src/cli.ts
@@ -3,84 +3,88 @@
 
 import * as cmd from 'commander';
 
-import {Cache, FetchOptions} from 'cget';
+import { Cache, FetchOptions } from 'cget';
 import * as cxml from 'cxml';
 
-import {Context} from './xsd/Context';
-import {Namespace} from './xsd/Namespace';
-import {Loader} from './xsd/Loader';
-import {exportNamespace} from './xsd/Exporter';
+import { Context } from './xsd/Context';
+import { Namespace } from './xsd/Namespace';
+import { Loader } from './xsd/Loader';
+import { exportNamespace } from './xsd/Exporter';
 import * as schema from './schema';
-import {AddImports} from './schema/transform/AddImports';
-import {Sanitize} from './schema/transform/Sanitize';
+import { AddImports } from './schema/transform/AddImports';
+import { Sanitize } from './schema/transform/Sanitize';
 
 type _ICommand = typeof cmd;
 interface ICommand extends _ICommand {
-	arguments(spec: string): ICommand;
+  arguments(spec: string): ICommand;
 }
 
-((cmd.version(require('../package.json').version) as ICommand)
-	.arguments('<url>')
-	.description('XSD download and conversion tool')
-	.option('-H, --force-host <host>', 'Fetch all xsd files from <host>\n    (original host is passed in GET parameter "host")')
-	.option('-P, --force-port <port>', 'Connect to <port> when using --force-host')
-	// .option('-c, --cache-xsd <path>', 'Cache downloaded XSD filed under <path>')
-	.option('-t, --out-ts <path>', 'Output TypeScript definitions under <path>')
-	.option('-j, --out-js <path>', 'Output JavaScript modules under <path>')
-	.action(handleConvert)
-	.parse(process.argv)
-);
+(cmd.version(require('../package.json').version) as ICommand)
+  .arguments('<url>')
+  .description('XSD download and conversion tool')
+  .option(
+    '-H, --force-host <host>',
+    'Fetch all xsd files from <host>\n    (original host is passed in GET parameter "host")'
+  )
+  .option(
+    '-P, --force-port <port>',
+    'Connect to <port> when using --force-host'
+  )
+  // .option('-c, --cache-xsd <path>', 'Cache downloaded XSD filed under <path>')
+  .option('-t, --out-ts <path>', 'Output TypeScript definitions under <path>')
+  .option('-j, --out-js <path>', 'Output JavaScript modules under <path>')
+  .action(handleConvert)
+  .parse(process.argv);
 
-if(process.argv.length < 3) cmd.help();
+if (process.argv.length < 3) cmd.help();
 
 function handleConvert(urlRemote: string, opts: { [key: string]: any }) {
-	var schemaContext = new schema.Context();
-	var xsdContext = new Context(schemaContext);
+  var schemaContext = new schema.Context();
+  var xsdContext = new Context(schemaContext);
 
-	var fetchOptions: FetchOptions = {};
+  var fetchOptions: FetchOptions = {};
 
-	if(opts['forceHost']) {
-		fetchOptions.forceHost = opts['forceHost'];
-		if(opts['forcePort']) fetchOptions.forcePort = opts['forcePort'];
+  if (opts['forceHost']) {
+    fetchOptions.forceHost = opts['forceHost'];
+    if (opts['forcePort']) fetchOptions.forcePort = opts['forcePort'];
 
-		Cache.patchRequest();
-	}
+    Cache.patchRequest();
+  }
 
-	var jsCache = new Cache(opts['outJs'] || 'xmlns', '_index.js');
-	var tsCache = new Cache(opts['outTs'] || 'xmlns', '_index.d.ts');
+  var jsCache = new Cache(opts['outJs'] || 'xmlns', '_index.js');
+  var tsCache = new Cache(opts['outTs'] || 'xmlns', '_index.d.ts');
 
-	var loader = new Loader(xsdContext, fetchOptions);
+  var loader = new Loader(xsdContext, fetchOptions);
 
-	loader.import(urlRemote).then((namespace: Namespace) => {
-		try {
-			exportNamespace(xsdContext.primitiveSpace, schemaContext);
-			exportNamespace(xsdContext.xmlSpace, schemaContext);
+  loader.import(urlRemote).then((namespace: Namespace) => {
+    try {
+      exportNamespace(xsdContext.primitiveSpace, schemaContext);
+      exportNamespace(xsdContext.xmlSpace, schemaContext);
 
-			var spec = exportNamespace(namespace, schemaContext);
+      var spec = exportNamespace(namespace, schemaContext);
 
-			var addImports = new AddImports(spec);
-			var sanitize = new Sanitize(spec);
+      var addImports = new AddImports(spec);
+      var sanitize = new Sanitize(spec);
 
-			var importsAdded = addImports.exec();
+      var importsAdded = addImports.exec();
 
-			// Find ID numbers of all types imported from other namespaces.
-			importsAdded.then(() =>
-				// Rename types to valid JavaScript class names,
-				// adding a prefix or suffix to duplicates.
-				sanitize.exec()
-			).then(() =>
-				sanitize.finish()
-			).then(() =>
-				addImports.finish(importsAdded.value())
-			).then(() =>
-				new schema.exporter.JS(spec, jsCache).exec()
-			).then(() =>
-				new schema.exporter.TS(spec, tsCache).exec()
-			);
-		} catch(err) {
-			console.error(err);
-			console.log('Stack:');
-			console.error(err.stack);
-		}
-	});
+      // Find ID numbers of all types imported from other namespaces.
+      importsAdded
+        .then(() =>
+          // Rename types to valid JavaScript class names,
+          // adding a prefix or suffix to duplicates.
+          sanitize.exec()
+        )
+        .then(() => sanitize.finish())
+        .then(() => addImports.finish(importsAdded.value()))
+        // .then(() =>
+        // new schema.exporter.JS(spec, jsCache).exec()
+        // )
+        .then(() => new schema.exporter.TS(spec, tsCache).exec());
+    } catch (err) {
+      console.error(err);
+      console.log('Stack:');
+      console.error(err.stack);
+    }
+  });
 }
